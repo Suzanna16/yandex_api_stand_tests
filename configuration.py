5a7d229a9655035ef70270494812c53aed7900f8
Suzanna <shirinyan.s16@yandex.ru>
2023-12-01 08:43:10 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://c311d7bc-43de-46a0-a84a-183d9ff7170b.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
